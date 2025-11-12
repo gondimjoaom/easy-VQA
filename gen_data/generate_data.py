0c378802f24437ebe6fe1fd584436a4d089653cc
João Gondim <gondimjoaom@gmail.com>
2025-11-12 16:22:20 -0300

    removing old dataset files and adding new code for location of shapes

--- gen_data/generate_data.py
+++ gen_data/generate_data.py
@@ -2,46 +2,50 @@ from shape import Shape
 from color import Color
 from images import create_image
 from questions import create_questions
-from random import choice
+from random import choice, seed
 import json
-import os
+import os, sys
 
-if not os.path.exists('easy_vqa/data/train/images'):
+if not os.path.exists('../easy_vqa/data/train/images'):
   os.makedirs('easy_vqa/data/train/images/')
-if not os.path.exists('easy_vqa/data/test/images'):
+if not os.path.exists('../easy_vqa/data/test/images'):
   os.makedirs('easy_vqa/data/test/images/')
 
 colors = list(Color)
 shapes = list(Shape)
 
-NUM_TRAIN = 4000
-NUM_TEST = 1000
+NUM_TRAIN = 4000 if len(sys.argv) == 1 else int(sys.argv[1])
+NUM_TEST = 1000 if len(sys.argv) == 1 else int(sys.argv[2])
+location = None if len(sys.argv) == 1 else str(sys.argv[3])
+seed(198)
 
 def create_data(image_path, num):
   qs = []
   num_yes_no = 0
   for i in range(num):
     shape = choice(shapes)
+    shape = Shape.CIRCLE
+    # print(shape)  
     color = choice(colors)
 
-    create_image(f'{image_path}/{i}.png', shape, color)
+    create_image(f'{image_path}/{i}.png', shape, color, location=location)
     new_qs, new_num_yes_no = create_questions(shape, color, i)
     qs += new_qs
     num_yes_no += new_num_yes_no
   return qs, num_yes_no
 
-train_questions, num_train_yes_no = create_data('easy_vqa/data/train/images', NUM_TRAIN)
-test_questions, num_test_yes_no = create_data('easy_vqa/data/test/images', NUM_TEST)
+train_questions, num_train_yes_no = create_data('../easy_vqa/data/train/images', NUM_TRAIN)
+test_questions, num_test_yes_no = create_data('../easy_vqa/data/test/images', NUM_TEST)
 
 all_questions = train_questions + test_questions
 all_answers = list(set(map(lambda q: q[1], all_questions)))
 
-with open('easy_vqa/data/train/questions.json', 'w') as file:
-  json.dump(train_questions, file)
-with open('easy_vqa/data/test/questions.json', 'w') as file:
-  json.dump(test_questions, file)
+with open('../easy_vqa/data/train/questions.json', 'w') as file:
+  json.dump(train_questions, file, indent=4)
+with open('../easy_vqa/data/test/questions.json', 'w') as file:
+  json.dump(test_questions, file, indent=4)
 
-with open('easy_vqa/data/answers.txt', 'w') as file:
+with open('../easy_vqa/data/answers.txt', 'w') as file:
   for answer in all_answers:
     file.write(f'{answer}\n')
 
--- gen_data/images.py
+++ gen_data/images.py
@@ -15,43 +15,72 @@ MAX_SHAPE_SIZE = IM_DRAW_SIZE / 2
 TRIANGLE_ANGLE_1 = 0
 TRIANGLE_ANGLE_2 = -math.pi / 3
 
-def create_image(filename, shape, color):
-  r = randint(230, 255)
-  g = randint(230, 255)
-  b = randint(230, 255)
-  im = Image.new('RGB', (IM_DRAW_SIZE, IM_DRAW_SIZE), (r, g, b))
+def create_image(filename, shape, color, location=None):
+	r = randint(230, 255)
+	g = randint(230, 255)
+	b = randint(230, 255)
+	im = Image.new('RGB', (IM_DRAW_SIZE, IM_DRAW_SIZE), (r, g, b))
 
-  draw = ImageDraw.Draw(im)
-  draw_shape(draw, shape, color)
-  del draw
+	draw = ImageDraw.Draw(im)
+	draw_shape(draw, shape, color, location)
+	del draw
 
-  im = im.resize((IM_SIZE, IM_SIZE), resample=Image.BILINEAR)
+	im = im.resize((IM_SIZE, IM_SIZE), resample=Image.BILINEAR)
 
-  im.save(filename, 'png')
+	im.save(filename, 'png')
 
-def draw_shape(draw, shape, color):
-  if shape is Shape.RECTANGLE:
-    w = randint(MIN_SHAPE_SIZE, MAX_SHAPE_SIZE)
-    h = randint(MIN_SHAPE_SIZE, MAX_SHAPE_SIZE)
-    x = randint(0, IM_DRAW_SIZE - w)
-    y = randint(0, IM_DRAW_SIZE - h)
-    draw.rectangle([(x, y), (x + w, y + h)], fill=color.value)
+def draw_shape(draw, shape, color, location = None):
+	if shape is Shape.RECTANGLE:
+		if location == "left":
+			w = randint(MIN_SHAPE_SIZE, MAX_SHAPE_SIZE / 2) # lowering max shape size to ensure
+			h = randint(MIN_SHAPE_SIZE, MAX_SHAPE_SIZE / 2) # it will fit in the image
+			x = randint(0, IM_DRAW_SIZE // 2 - w)
+			y = randint(0, IM_DRAW_SIZE - h)
+			draw.rectangle([(x, y), (x + w, y + h)], fill=color.value)
+		elif location == "right":
+			w = randint(MIN_SHAPE_SIZE, MAX_SHAPE_SIZE / 2) # lowering max shape size to ensure
+			h = randint(MIN_SHAPE_SIZE, MAX_SHAPE_SIZE / 2) # it will fit in the image
+			x = randint(IM_DRAW_SIZE // 2 - w, IM_DRAW_SIZE)
+			y = randint(0, IM_DRAW_SIZE - h)
+			draw.rectangle([(x, y), (x + w, y + h)], fill=color.value)
+		else:
+			w = randint(MIN_SHAPE_SIZE, MAX_SHAPE_SIZE)
+			h = randint(MIN_SHAPE_SIZE, MAX_SHAPE_SIZE)
+			x = randint(0, IM_DRAW_SIZE - w)
+			y = randint(0, IM_DRAW_SIZE - h)
+			draw.rectangle([(x, y), (x + w, y + h)], fill=color.value)
 
-  elif shape is Shape.CIRCLE:
-    d = randint(MIN_SHAPE_SIZE, MAX_SHAPE_SIZE)
-    x = randint(0, IM_DRAW_SIZE - d)
-    y = randint(0, IM_DRAW_SIZE - d)
-    draw.ellipse([(x, y), (x + d, y + d)], fill=color.value)
+	elif shape is Shape.CIRCLE:
+		if location == "left":
+			d = randint(MIN_SHAPE_SIZE, MAX_SHAPE_SIZE / 2) # lowering max shape size to ensure
+															# it will fit in the image
+			x = randint(0, IM_DRAW_SIZE // 2  - d)
+			y = randint(0, IM_DRAW_SIZE - d)
+			draw.ellipse([(x, y), (x + d, y + d)], fill=color.value)
+		elif location == "right":
+			d = randint(MIN_SHAPE_SIZE, MAX_SHAPE_SIZE / 2) # lowering max shape size to ensure
+															# it will fit in the image
+			x = randint(IM_DRAW_SIZE // 2  - d, IM_DRAW_SIZE)
+			y = randint(0, IM_DRAW_SIZE - d)
+			draw.ellipse([(x, y), (x + d, y + d)], fill=color.value)
+		else:
+			d = randint(MIN_SHAPE_SIZE, MAX_SHAPE_SIZE)
+			x = randint(0, IM_DRAW_SIZE - d)
+			y = randint(0, IM_DRAW_SIZE - d)
+			draw.ellipse([(x, y), (x + d, y + d)], fill=color.value)
 
-  elif shape is Shape.TRIANGLE:
-    s = randint(MIN_SHAPE_SIZE, MAX_SHAPE_SIZE)
-    x = randint(0, IM_DRAW_SIZE - s)
-    y = randint(math.ceil(s * math.sin(math.pi / 3)), IM_DRAW_SIZE)
-    draw.polygon([
-      (x, y),
-      (x + s * math.cos(TRIANGLE_ANGLE_1), y + s * math.sin(TRIANGLE_ANGLE_1)),
-      (x + s * math.cos(TRIANGLE_ANGLE_2), y + s * math.sin(TRIANGLE_ANGLE_2)),
-    ], fill=color.value)
+	elif shape is Shape.TRIANGLE:
+		if location == "left":
+			pass
+		else:
+			s = randint(MIN_SHAPE_SIZE, MAX_SHAPE_SIZE)
+			x = randint(0, IM_DRAW_SIZE - s)
+			y = randint(math.ceil(s * math.sin(math.pi / 3)), IM_DRAW_SIZE)
+			draw.polygon([
+				(x, y),
+				(x + s * math.cos(TRIANGLE_ANGLE_1), y + s * math.sin(TRIANGLE_ANGLE_1)),
+				(x + s * math.cos(TRIANGLE_ANGLE_2), y + s * math.sin(TRIANGLE_ANGLE_2)),
+			], fill=color.value)
 
-  else:
-    raise Exception('Invalid shape!')
+	else:
+		raise Exception('Invalid shape!')
